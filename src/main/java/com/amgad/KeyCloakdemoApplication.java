d1e4598b7c99677a744690e54c84d2ddd6884f5b
amgad-mohammed <amgad.mohammed2019@gmail.com>
2025-12-27 18:51:31 +0200

    key-cloak spring boot integration

---
+++ src/main/java/com/amgad/KeyCloakdemoApplication.java
@@ -0,0 +1,13 @@
+package com.amgad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloakdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyCloakdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amgad/KeyCloakdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amgad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyCloakdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
